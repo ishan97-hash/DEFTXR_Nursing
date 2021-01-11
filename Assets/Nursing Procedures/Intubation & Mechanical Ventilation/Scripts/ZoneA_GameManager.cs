2e30148b3d76fd362e5b58277ac9194b242e438c
Sanil Shah <sanilshah1999@gmail.com>
2021-01-11 16:06:28 +0530

    Sanil Shah: Updated VO Script with Actions
    Updated Vo Script with actions.
    Added Grabbable Objects.

--- Assets/Nursing Procedures/Intubation & Mechanical Ventilation/Scripts/ZoneA_GameManager.cs
+++ Assets/Nursing Procedures/Intubation & Mechanical Ventilation/Scripts/ZoneA_GameManager.cs
@@ -8,9 +8,9 @@ public class ZoneA_GameManager : MonoBehaviour
     [SerializeField]
     private List<AudioClip> intro_VO;
 
-    // Actions referencing Actions to be done by User
+    // Actions referencing Actions to be completed by user
     [SerializeField]
-    private List<GameObject> Actions;
+    private bool[] ActionsCompleted = {false};
 
     // Guides referencing Guide Animation to be displayed on each step
     [SerializeField]
@@ -23,58 +23,101 @@ public class ZoneA_GameManager : MonoBehaviour
     [SerializeField]
     private GameObject patient;
 
+    private bool grabbedOnce1 = false;
+    private bool grabbedOnce2 = false;
+    private bool grabbedOnce3 = false;
+    private bool grabbedOnce4 = false;
+
+
+    // Grabbable Apparatus Eg. Scissor, Syringe
+    [SerializeField]
+    private OVRGrabbable grabbableObject1;
+    [SerializeField]
+    private OVRGrabbable grabbableObject2;
+    [SerializeField]
+    private OVRGrabbable grabbableObject3;
+    [SerializeField]
+    private OVRGrabbable grabbableObject4;
+
     private void Start()
     {
         InitializeDefaultData();
-        StartCoroutine(Introduction());
+        //StartCoroutine(Introduction());
     }
 
     void Update()
     {
-        
+        if(grabbableObject1.isGrabbed == true && ActionsCompleted[1] == false)
+        {
+            StartCoroutine(Step1());
+            ActionsCompleted[1] = true;
+            grabbableObject2.GetComponent<BoxCollider>().enabled = true;
+        }
+        if (grabbableObject2.isGrabbed == true && ActionsCompleted[2] == false)
+        {
+            StartCoroutine(Step2());
+            ActionsCompleted[2] = true;
+            grabbableObject3.GetComponent<BoxCollider>().enabled = true;
+        }
+        if (grabbableObject3.isGrabbed == true && ActionsCompleted[3] == false)
+        {
+            StartCoroutine(Step3());
+            ActionsCompleted[3] = true;
+            grabbableObject4.GetComponent<BoxCollider>().enabled = true;
+        }
+        if (grabbableObject4.isGrabbed == true && ActionsCompleted[4] == false)
+        {
+            StartCoroutine(Step4());
+            ActionsCompleted[4] = true;
+        }
     }
 
     void InitializeDefaultData()
     {
         // Disable all models
-        patient.SetActive(false);
+        //patient.SetActive(false);
 
         for (int i=0; i<Guides.Count; i++)
         {
             Guides[i].SetActive(false);
         }
 
+        //Disable all Interactables/Grabbable property of GrabbableObjects, except 1st
+        grabbableObject2.GetComponent<BoxCollider>().enabled = false;
+        grabbableObject3.GetComponent<BoxCollider>().enabled = false;
+        grabbableObject4.GetComponent<BoxCollider>().enabled = false;
     }
 
-    IEnumerator Introduction()
+    IEnumerator Step1()
     {
-        // activate the required component
-
-        yield return new WaitForSeconds(2f);
-
-        // Show Patient
-        patient.SetActive(true);
 
         // Step 1
-        audioSource.PlayOneShot(intro_VO[0]);
-        Guides[0].SetActive(true);
-        yield return new WaitForSeconds(intro_VO[0].length);
+            audioSource.PlayOneShot(intro_VO[0]);
+            Guides[0].SetActive(true);
+            yield return new WaitForSeconds(intro_VO[0].length);
 
+    }
+
+     IEnumerator Step2()
+    {
         // Step 2
         audioSource.PlayOneShot(intro_VO[1]);
-        Guides[0].SetActive(false);
         Guides[1].SetActive(true);
         yield return new WaitForSeconds(intro_VO[1].length);
+    }
 
-        // Step 3
+    IEnumerator Step3()
+    {
+        // Step 2
         audioSource.PlayOneShot(intro_VO[2]);
-        Guides[1].SetActive(false);
         Guides[2].SetActive(true);
         yield return new WaitForSeconds(intro_VO[2].length);
+    }
 
-        // Step 4
+    IEnumerator Step4()
+    {
+        // Step 2
         audioSource.PlayOneShot(intro_VO[3]);
-        Guides[2].SetActive(false);
         Guides[3].SetActive(true);
         yield return new WaitForSeconds(intro_VO[3].length);
     }
